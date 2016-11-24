b74d3a25b7641b7ef8e2ad5526375781ef095826
fangzhen <1090572028@qq.com>
2016-11-24 22:31:39 +0800

    feat :  modal

--- react/component/LOL/myDetail.js
+++ react/component/LOL/myDetail.js
@@ -9,7 +9,6 @@ import { Kohana } from 'react-native-textinput-effects';
 
 var Modal   = require('react-native-modalbox');
 
-const DEMO_OPTIONS_1 = ['option 1', 'option 2', 'option 3', 'option 4', 'option 5', 'option 6', 'option 7', 'option 8', 'option 9'];
 
 export default class Demo extends Component {
 
@@ -18,7 +17,12 @@ export default class Demo extends Component {
 
         this.state = {
             swipeToClose:true,
+            selected_title:'选择大区',
+            selected_area_id:'',
+            area_list:[],
         };
+
+        this._getAreaList();
 
         setTimeout(()=>{
             this.openModal();
@@ -31,6 +35,58 @@ export default class Demo extends Component {
 
     closeModal =()=>{
         this.refs.modal.close();
+    }
+
+
+    /**
+     * 获取LOL大区列表
+     * @private
+     */
+    _getAreaList = ()=>{
+        let fetchUtil = new FetchUtil();
+        fetchUtil.init()
+            .setUrl(URL.LOL_AREA)
+            .setMethod('GET')
+            .setHeader({
+                'DAIWAN-API-TOKEN':KEY.LOL_API_KEY
+            })
+            .dofetch()
+            .then((data)=>{
+                let area_arr = [];
+
+                for(let item of data.data){
+                    area_arr.push(item.name);
+                }
+
+                this.setState({
+                    area_list:area_arr,
+                });
+            })
+            .catch((error)=>{
+                alert('error:'+error);
+            })
+    }
+
+    /**
+     * 选中大区
+     * @param index
+     * @param value
+     * @private
+     */
+    _handleSelected=(index,value)=>{
+        this.setState({
+            selected_title:value,
+            selected_area_id:index,
+        });
+    }
+
+    _handleSure = ()=>{
+        if(this.state.selected_area_id&&this.state.selected_title){
+            this.refs.modal.close();
+        }
+        else {
+            ToastAndroid.show('请选择ID和大区',ToastAndroid.SHORT);
+        }
     }
 
     render() {
@@ -48,6 +104,8 @@ export default class Demo extends Component {
                         backdropOpacity={0.1}
                         backdropContent={<Text></Text>}
                         animationDuration={800}
+                        backdropPressToClose={false}
+                        entry="top"
                     >
                         <View style={{flex:1}}>
 
@@ -66,18 +124,24 @@ export default class Demo extends Component {
                                 </View>
 
                                 <View style={{flex:1,margin:5}}>
-                                    <ModalDropdown options={DEMO_OPTIONS_1}  dropdownStyle={{backgroundColor:'#f9f5ed',width:200}} >
+                                    <ModalDropdown
+                                        options={this.state.area_list}
+                                        dropdownStyle={{backgroundColor:'#f9f5ed',flex:1,width:SceneWidth-10,alignItems:'center',justifyContent:'center'}}
+                                        onSelect={this._handleSelected}
+                                        textStyle={{fontSize:20}}
+                                    >
                                         <View style={{backgroundColor: '#f9f5ed',borderWidth:1,borderRadius:5,borderColor:'#d4d4d3',height:50,justifyContent:'center'}}>
-                                            <Text style={{color:'#91627b',fontSize:18,fontWeight:'bold',marginLeft:15}}>选择大区</Text>
+                                            <Text style={{color:'#91627b',fontSize:18,fontWeight:'bold',marginLeft:15}}>{this.state.selected_title}</Text>
                                         </View>
                                     </ModalDropdown>
                                 </View>
 
                             </View>
 
-
-                            <View>
-                                <TouchableOpacity onPress={()=>{this.closeModal()}} style={styles.btn}><Text>确定</Text></TouchableOpacity>
+                            <View style={{alignItems:'center',margin:10}}>
+                                <View style={{width:300,height:50,alignItems:'center',backgroundColor:'#6bbb58',justifyContent:'center',borderRadius:10}}>
+                                    <TouchableOpacity onPress={()=>{this._handleSure()}}><Text style={{fontSize:25,color:'#fff'}}>绑定</Text></TouchableOpacity>
+                                </View>
                             </View>
                         </View>
 
