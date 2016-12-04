656ed419cf75dac53759f949d8627ff22479a311
fangzhen <1090572028@qq.com>
2016-12-04 22:52:21 +0800

    feat :  详情页优化

--- react/component/LOL/gameDetail.js
+++ react/component/LOL/gameDetail.js
@@ -8,6 +8,7 @@ import LoadingPage from './../../common/LoadingPage/index';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 
+import ChampionInfo from './championInfo';
 
 const game_type = ['','自定义','新手关','匹配赛','排位赛','战队赛','大乱斗','人机','统治战场','大对决'];
 game_type[24] = '火力赛';
@@ -68,9 +69,10 @@ export default class GameDetail extends Component{
                 <LoadingPage title="战绩加载中..."/>
             );
         }
-        var players = [];
-        this.state.game_detail.gamer_records.map((item, i)=> {
+        var win_players = [];
+        var failed_players = [];
 
+        this.state.game_detail.gamer_records.map((item, i)=> {
             let extend_icon = this.state.item_is_extend[i]?"angle-up":"angle-down";
             let extend_view = this.state.item_is_extend[i]?(<View style={{flex:1,flexDirection:'column',backgroundColor:'#eee',marginRight:5,marginLeft:5}}>
                     <View style={{flex:1,flexDirection:'row'}}>
@@ -97,65 +99,185 @@ export default class GameDetail extends Component{
                 </View>)
                 :null;
 
-            players.push(
-                <View key={i} style={{flex:1,flexDirection:'column',margin:5}}>
 
-                <View key={i} style={{flex: 1, flexDirection: 'row',margin:5}}>
-                    <View style={{flex: 1}}>
-                        <Image
-                            source={{uri: 'http://cdn.tgp.qq.com/pallas/images/champions_id/' + item.champion_id + '.png'}}
-                            style={{width: 50, height: 50}}></Image>
-                    </View>
-                    <View style={{flex:3,flexDirection:'column'}}>
-                        <View style={{flexDirection:'row'}}>
-                            <View style={{flex:1}}>
-                                <Text>{item.name}</Text>
-                            </View>
-                            <View style={{flex:1}}>
-                                <Text>{item.champions_killed}/{item.num_deaths}/{item.assists}</Text>
-                            </View>
-                        </View>
-                        <View style={{flex:1,flexDirection:'row'}}>
-                            <Image style={{width:30,height:30}} source={{uri:'http://ddragon.leagueoflegends.com/cdn/6.21.1/img/item/'+item.item0+'.png'}}></Image>
-                            <Image style={{width:30,height:30}} source={{uri:'http://ddragon.leagueoflegends.com/cdn/6.21.1/img/item/'+item.item1+'.png'}}></Image>
-                            <Image style={{width:30,height:30}} source={{uri:'http://ddragon.leagueoflegends.com/cdn/6.21.1/img/item/'+item.item2+'.png'}}></Image>
-                            <Image style={{width:30,height:30}} source={{uri:'http://ddragon.leagueoflegends.com/cdn/6.21.1/img/item/'+item.item3+'.png'}}></Image>
-                            <Image style={{width:30,height:30}} source={{uri:'http://ddragon.leagueoflegends.com/cdn/6.21.1/img/item/'+item.item4+'.png'}}></Image>
-                            <Image style={{width:30,height:30}} source={{uri:'http://ddragon.leagueoflegends.com/cdn/6.21.1/img/item/'+item.item5+'.png'}}></Image>
-                        </View>
-                        <TouchableOpacity style={{flex:1,alignItems:'flex-end'}} onPress={()=> {
-                            let arr = this.state.item_is_extend;
-                            arr[i] = !arr[i];
-                            this.setState({
-                                item_is_extend: arr
-                            });
-                        }}>
-                            <View>
-                                <Icon name={extend_icon} size={20}/>
-                            </View>
-                        </TouchableOpacity>
-                    </View>
+            //胜利队伍
+            if(item.win==1){
+                win_players.push(
+                    <View key={i} style={{flex:1,flexDirection:'column',margin:5,borderBottomWidth:1,borderBottomColor:'#eee'}}>
 
-                </View>
-                    {extend_view}
-                </View>
-            );
+                        <View key={i} style={{flex: 1, flexDirection: 'row',margin:5}}>
+                            <TouchableOpacity style={{flex:1}} onPress={()=>{
+                                this.props.navigator.push({name:'championInfo',component:ChampionInfo,param:{id:item.champion_id,navigator:this.props.navigator}});
+                            }}>
+                                <View style={{flex: 1,alignItems:'center'}}>
+                                    <Image
+                                        source={{uri: 'http://cdn.tgp.qq.com/pallas/images/champions_id/' + item.champion_id + '.png'}}
+                                        style={{width: 60, height: 60,marginRight:5}}></Image>
+                                </View>
+                            </TouchableOpacity>
+
+                            <View style={{flex:3,flexDirection:'column'}}>
+
+                                <View style={{flexDirection:'row'}}>
+                                    <View style={{flex:3}}>
+                                        <TouchableOpacity onPress={()=>{
+
+                                        }}>
+                                            <Text style={{fontSize:20,fontWeight:'bold'}}>{item.name}</Text>
+                                        </TouchableOpacity>
+                                    </View>
+
+                                    <View style={{flex:1,alignItems:'center'}}>
+                                        <Text style={{fontSize:15}}>Lv {item.level}</Text>
+                                    </View>
+
+                                    <View style={{flex:1,alignItems:'flex-end'}}>
+                                        <Text style={{fontSize:15}}>{item.champions_killed}/{item.num_deaths}/{item.assists}</Text>
+                                    </View>
+
+                                </View>
+
+                                <View style={{flex:1,flexDirection:'row'}}>
+                                    <View style={{borderWidth:1,borderColor:'#bbb4b4',margin:2}}>
+                                        <Image style={{width:30,height:30}} source={{uri:'http://ddragon.leagueoflegends.com/cdn/6.21.1/img/item/'+item.item0+'.png'}}></Image>
+                                    </View>
+                                    <View style={{borderWidth:1,borderColor:'#bbb4b4',margin:2}}>
+                                        <Image style={{width:30,height:30}} source={{uri:'http://ddragon.leagueoflegends.com/cdn/6.21.1/img/item/'+item.item1+'.png'}}></Image>
+                                    </View>
+                                    <View style={{borderWidth:1,borderColor:'#bbb4b4',margin:2}}>
+                                        <Image style={{width:30,height:30}} source={{uri:'http://ddragon.leagueoflegends.com/cdn/6.21.1/img/item/'+item.item2+'.png'}}></Image>
+                                    </View>
+                                    <View style={{borderWidth:1,borderColor:'#bbb4b4',margin:2}}>
+                                        <Image style={{width:30,height:30}} source={{uri:'http://ddragon.leagueoflegends.com/cdn/6.21.1/img/item/'+item.item3+'.png'}}></Image>
+                                    </View>
+                                    <View style={{borderWidth:1,borderColor:'#bbb4b4',margin:2}}>
+                                        <Image style={{width:30,height:30}} source={{uri:'http://ddragon.leagueoflegends.com/cdn/6.21.1/img/item/'+item.item4+'.png'}}></Image>
+                                    </View>
+                                    <View style={{borderWidth:1,borderColor:'#bbb4b4',margin:2}}>
+                                        <Image style={{width:30,height:30}} source={{uri:'http://ddragon.leagueoflegends.com/cdn/6.21.1/img/item/'+item.item5+'.png'}}></Image>
+                                    </View>
+
+                                </View>
+                                <TouchableOpacity style={{flex:1,alignItems:'flex-end'}} onPress={()=> {
+                                    let arr = this.state.item_is_extend;
+                                    arr[i] = !arr[i];
+                                    this.setState({
+                                        item_is_extend: arr
+                                    });
+                                }}>
+                                    <View>
+                                        <Icon name={extend_icon} size={20}/>
+                                    </View>
+                                </TouchableOpacity>
+                            </View>
+
+                        </View>
+                        {extend_view}
+                    </View>
+                );
+            }
+            else {
+                failed_players.push(
+                    <View key={i} style={{flex:1,flexDirection:'column',margin:5,borderBottomWidth:1,borderBottomColor:'#eee'}}>
+
+                        <View key={i} style={{flex: 1, flexDirection: 'row',margin:5}}>
+
+                            <TouchableOpacity style={{flex:1}} onPress={()=>{
+                                this.props.navigator.push({name:'championInfo',component:ChampionInfo,param:{id:item.champion_id,navigator:this.props.navigator}});
+                            }}>
+                                <View style={{flex: 1,alignItems:'center'}}>
+                                    <Image
+                                        source={{uri: 'http://cdn.tgp.qq.com/pallas/images/champions_id/' + item.champion_id + '.png'}}
+                                        style={{width: 60, height: 60,marginRight:5}}></Image>
+                                </View>
+                            </TouchableOpacity>
+
+                            <View style={{flex:3,flexDirection:'column'}}>
+
+                                <View style={{flexDirection:'row'}}>
+                                    <View style={{flex:3}}>
+                                        <TouchableOpacity onPress={()=>{
+
+                                        }}>
+                                            <Text style={{fontSize:20,fontWeight:'bold'}}>{item.name}</Text>
+                                        </TouchableOpacity>
+                                    </View>
+
+                                    <View style={{flex:1,alignItems:'center'}}>
+                                        <Text style={{fontSize:15}}>Lv {item.level}</Text>
+                                    </View>
+
+                                    <View style={{flex:1,alignItems:'flex-end'}}>
+                                        <Text style={{fontSize:15}}>{item.champions_killed}/{item.num_deaths}/{item.assists}</Text>
+                                    </View>
+
+                                </View>
+
+                                <View style={{flex:1,flexDirection:'row'}}>
+                                    <View style={{borderWidth:1,borderColor:'#bbb4b4',margin:2}}>
+                                        <Image style={{width:30,height:30}} source={{uri:'http://ddragon.leagueoflegends.com/cdn/6.21.1/img/item/'+item.item0+'.png'}}></Image>
+                                    </View>
+                                    <View style={{borderWidth:1,borderColor:'#bbb4b4',margin:2}}>
+                                        <Image style={{width:30,height:30}} source={{uri:'http://ddragon.leagueoflegends.com/cdn/6.21.1/img/item/'+item.item1+'.png'}}></Image>
+                                    </View>
+                                    <View style={{borderWidth:1,borderColor:'#bbb4b4',margin:2}}>
+                                        <Image style={{width:30,height:30}} source={{uri:'http://ddragon.leagueoflegends.com/cdn/6.21.1/img/item/'+item.item2+'.png'}}></Image>
+                                    </View>
+                                    <View style={{borderWidth:1,borderColor:'#bbb4b4',margin:2}}>
+                                        <Image style={{width:30,height:30}} source={{uri:'http://ddragon.leagueoflegends.com/cdn/6.21.1/img/item/'+item.item3+'.png'}}></Image>
+                                    </View>
+                                    <View style={{borderWidth:1,borderColor:'#bbb4b4',margin:2}}>
+                                        <Image style={{width:30,height:30}} source={{uri:'http://ddragon.leagueoflegends.com/cdn/6.21.1/img/item/'+item.item4+'.png'}}></Image>
+                                    </View>
+                                    <View style={{borderWidth:1,borderColor:'#bbb4b4',margin:2}}>
+                                        <Image style={{width:30,height:30}} source={{uri:'http://ddragon.leagueoflegends.com/cdn/6.21.1/img/item/'+item.item5+'.png'}}></Image>
+                                    </View>
+                                </View>
+                                <TouchableOpacity style={{flex:1,alignItems:'flex-end'}} onPress={()=> {
+                                    let arr = this.state.item_is_extend;
+                                    arr[i] = !arr[i];
+                                    this.setState({
+                                        item_is_extend: arr
+                                    });
+                                }}>
+                                    <View>
+                                        <Icon name={extend_icon} size={20}/>
+                                    </View>
+                                </TouchableOpacity>
+                            </View>
+
+                        </View>
+                        {extend_view}
+                    </View>
+                );
+            }
+
+
         });
 
         return (
             <View style={{flex:1,backgroundColor:'#fff'}}>
                 <PageTitle title="比赛详情" navigator={this.props.navigator}/>
-                <View style={{flex:1}}>
-                    <View style={{flexDirection:'row',margin:5,alignItems:'center'}}>
-                        <View style={{flex:1}}><Text>开始时间  {this._cut_time(this.state.game_detail.start_time)}</Text></View>
-                        <View style={{flex:1}}><Text>历时  {parseInt(this.state.game_detail.duration/60)}分钟</Text></View>
-                        <View style={{flex:1}}><Text>模式  {game_type[this.state.game_detail.game_type]}</Text></View>
+                <View style={{flex:1,flexDirection:'column'}}>
+                    <View style={{alignItems:'center',justifyContent:'center'}}>
+                        <View style={{flexDirection:'row'}}>
+                            <View style={{flex:1,alignItems:'center'}}><Text>开始时间  {this._cut_time(this.state.game_detail.start_time)}</Text></View>
+                            <View style={{flex:1,alignItems:'center'}}><Text>历时  {parseInt(this.state.game_detail.duration/60)}分钟</Text></View>
+                            <View style={{flex:1,alignItems:'center'}}><Text>模式  {game_type[this.state.game_detail.game_type]}</Text></View>
+                        </View>
                     </View>
 
                     <View style={{flex:1}}>
                         <ScrollView>
                             <View style={{flex:1,flexDirection:'column'}}>
-                                {players}
+                                <View style={{width:100,height:40,backgroundColor:'#3dbbab',alignItems:'center',justifyContent:'center',marginTop:10}}>
+                                    <Text style={{color:'#fff',fontSize:20}}>胜利方</Text>
+                                </View>
+                                {win_players}
+                                <View style={{width:100,height:40,backgroundColor:'#cc3d37',alignItems:'center',justifyContent:'center',marginTop:10}}>
+                                    <Text style={{color:'#fff',fontSize:20}}>失败方</Text>
+                                </View>
+                                {failed_players}
                             </View>
 
                             <View></View>
