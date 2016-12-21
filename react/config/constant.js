83107d2e65eb7d0fade9a1d1ceeaeddaee84fae8
Jerry <1090572028@qq.com>
2016-12-21 09:44:46 +0800

    key

--- react/config/constant.js
+++ react/config/constant.js
@@ -5,5 +5,5 @@
 export default KEY = {
     BAIDU_API_KEY:'ae37157eec6f75ef47464959014db854',               //百度api的key
     JUHE_API_KEY:'e8c24a546c7ec420f532c0bce00a1f00',                //聚合api的key
-    LOL_API_KEY:'B4A22-02B47-CCDDB-40BC6',                          //LOL apikey
+    LOL_API_KEY:'6381A-8D343-631F6-3C3AC',                          //LOL apikey
 }
